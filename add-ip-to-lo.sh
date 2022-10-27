13b3e58921cabc607060d3c60b38bb1b2a0e5551
Mats Strålberg <msb@otto.local>
2022-10-27 13:17:30 +0200

    Doc files

---
+++ add-ip-to-lo.sh
@@ -0,0 +1,39 @@
+
+ip addr add 192.0.2.1/32 dev lo
+ip addr add 2001:db8::1/128 dev lo
+
+ip addr add 192.0.2.91/32 dev lo
+ip addr add 2001:db8::91/128 dev lo
+
+ip addr add 192.0.2.92/32 dev lo
+ip addr add 2001:db8::92/128 dev lo
+
+ip addr add 192.0.2.11/32 dev lo
+ip addr add 2001:db8::11/128 dev lo
+
+ip addr add 192.0.2.12/32 dev lo
+ip addr add 2001:db8::12/128 dev lo
+
+ip addr add 192.0.2.21/32 dev lo
+ip addr add 2001:db8::21/128 dev lo
+
+ip addr add 192.0.2.22/32 dev lo
+ip addr add 2001:db8::22/128 dev lo
+
+ip addr add 192.0.2.31/32 dev lo
+ip addr add 2001:db8::31/128 dev lo
+
+ip addr add 192.0.2.32/32 dev lo
+ip addr add 2001:db8::32/128 dev lo
+
+# edns0 OPT=99
+ip addr add 192.0.2.33/32 dev lo
+ip addr add 2001:db8::33/128 dev lo
+
+ip addr add 192.0.2.251/32 dev lo
+ip addr add 2001:db8::251/128 dev lo
+
+ip addr add 192.0.2.252/32 dev lo
+ip addr add 2001:db8::252/128 dev lo
+
+
